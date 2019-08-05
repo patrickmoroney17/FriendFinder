b72e1b977109b993a15b4af836f1b464261814dc
patrickmoroney17 <patrickmoroney17@gmail.com>
2019-08-05 17:40:18 -0400

    adding home page and intial data

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friendArray = [
+	{
+
+        "name":"Ahmed",
+        // "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ],  
+
+        "name":"Patrick",
+        //   "photo":"some pic.jpg",
+        "scores":[
+            "4",
+            "3",
+            "4",
+            "4",
+            "2",
+            "2",
+            "4",
+            "3",
+            "2",
+            "3"
+        ]
+	}
+];
+
+module.exports = friendArray;
